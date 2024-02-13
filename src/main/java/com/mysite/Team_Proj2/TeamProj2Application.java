aa2d06357efb58ab276b4e79da8cf29548513f52
Jangwoo Park <p.jangwoo@gmail>
2024-02-13 12:14:37 +0900

    초기 셋팅 커밋

---
+++ src/main/java/com/mysite/Team_Proj2/TeamProj2Application.java
@@ -0,0 +1,13 @@
+package com.mysite.Team_Proj2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProj2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamProj2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysite/Team_Proj2/TeamProj2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.Team_Proj2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProj2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
